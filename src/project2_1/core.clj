7e65b311268a7767eacacb5f5c065a542480dc62
Andy <ahayes5@oswego.edu>
2019-03-05 15:30:59 -0500

    HI

--- src/project2_1/core.clj
+++ src/project2_1/core.clj
@@ -18,23 +18,31 @@
   (or (= i true) (= i false))
   )
 
-(defn nand [i]
+;(defn nand [i]
+;  (cond
+;    (some false? i) '(true)
+;    (every? true? (rest i)) '(false)
+;    :else (if (not(every? boolean? (rest i)))
+;            (do (remove true? i));True if not all booleans
+;            ())))
+
+(defn nand [& args]
   (cond
-    (some false? i) '(true)
-    (every? true? (rest i)) '(false)
-    :else (if (not(every? boolean? (rest i)))
-            (do (remove true? i));True if not all booleans
-            ())))
+    (some false? args) true
+    (every? true? args) false
+    :else (if (not(every? boolean? args))
+            (concat '(nand) (remove true? args))
+            )))
 
 (def replacements {})
+
 (defn nand-simplify [input]
 
   (if (some seq? input)
     (map #(nand-simplify %) (filter seq? input))
-    (do (merge replacements {input (nand input)})))
+    (do (merge replacements {input (nand input)})))()
     (deep-substitute input replacements)
 
-  (nand input)
-  )
+  (nand input))
 
 (def test-list '(nand x y true (nand x true)))
