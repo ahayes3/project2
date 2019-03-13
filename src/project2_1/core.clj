83aec6893df482971ae4dc6425c7c99f57fb1112
Andy <ahayes5@oswego.edu>
2019-03-13 12:10:21 -0400

    FIXEDF AND

--- src/project2_1/core.clj
+++ src/project2_1/core.clj
@@ -1,46 +1,48 @@
 (ns project2-1.core)
 
 (defn lookup [i m]
-  "This function looks up a value, i, in map m and returns the result if it exists, and otherwise returns i."
-  (get m i i))
+	"This function looks up a value, i, in map m and returns the result if it exists, and otherwise returns i."
+	(get m i i))
 
 (defn substitute [l m]
-  (map (fn [i] (lookup i m)) l))
+	(map (fn [i] (lookup i m)) l))
 
 (defn deep-substitute [l m]
-  (map (fn [i]
-         (if (seq? i)
-           (deep-substitute i m)
-           (lookup i m)))
-       l))
+	(map (fn [i]
+				 (if (seq? i)
+					 (deep-substitute i m)
+					 (lookup i m)))
+			 l))
 
 (defn boolean? [i]
-  (or (= i true) (= i false))
-  )
+	(or (= i true) (= i false))
+	)
 
 (defn special-cases [l]
-;	(if (and (some seq? l) (=(count l) 2))
 
-;	)
+				 ;(if (and (= (count (rest l)) 1) (second l))
+				 ;
+					; )
+
 	)
 
 (defn nand-simplify [input]
 	(let [a (dedupe input)]
 		(if (some false?
-											(map (fn [i]
-														 (if (seq? i)
-															 (nand-simplify i)
-															 i)) a))
-			true
-			(if (every? true? (rest (map (fn [i]
+							(map (fn [i]
 										 (if (seq? i)
 											 (nand-simplify i)
-											 i)) a)))
+											 i)) a))
+			true
+			(if (every? true? (rest (map (fn [i]
+																		 (if (seq? i)
+																			 (nand-simplify i)
+																			 i)) a)))
 				false
 				(remove true? (map (fn [i]
-							 (if (seq? i)
-								 (nand-simplify i)
-								 i)) a))))))
+														 (if (seq? i)
+															 (nand-simplify i)
+															 i)) a))))))
 
 (defn not-nand [input]
 	(deep-substitute input {'not 'nand}))
@@ -48,25 +50,41 @@
 (defn or-nand [input]
 
 	(substitute (map (fn [i]
-				 (if (seq? i)
-					 (or-nand i)
-					 (if (= i 'or)
-						 'or
-						 (list 'nand i))
-					 )
+										 (if (seq? i)
+											 (or-nand i)
+											 (if (= i 'or)
+												 'or
+												 (if (= (first input) 'or)
+													 (list 'nand i)
+													 i))
+											 )
 
-				 ) input) {'or 'nand}))
+										 ) input) {'or 'nand}))
 
 (defn and-nand [input]
-	(list 'nand (substitute (map (fn [i]
-				 (if (seq? i)
-					 (and-nand i)
-					 i
-					 )
-				 )input) {'and 'nand})))
+	(if (= (first input) 'and)
+		(list 'nand (substitute (map (fn [i]
+																	 (if (seq? i)
+																		 (and-nand i)
+																		 i
+																		 )
+																	 ) input) {'and 'nand}))
+		(substitute (map (fn [i]
+																	 (if (seq? i)
+																		 (and-nand i)
+																		 i
+																		 )
+																	 ) input) {'and 'nand})
+
+		)
+	)
 
 (defn simplify-exp [input]
-	(and-nand(not-nand (or-nand input)))
+	(and-nand (not-nand (or-nand input)))
+	)
+
+(defn evalexp [exp bindings]
+	(nand-simplify (simplify-exp (deep-substitute exp bindings)))
 	)
 
 (def p1 '(and x (or x (and y (not z)))))
